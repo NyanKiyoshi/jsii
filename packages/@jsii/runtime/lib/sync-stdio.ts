6c7ef9d97f46537157031d51548ba8de480a59e8
Mikail KOCAK <mikail@mirumee.com>
2020-08-17 12:29:20 +0200

    Applied from upstream
    fix(@jsii/runtime): "maximum call stack size exceeded" in SyncStdio.readLine

--- packages/@jsii/runtime/lib/sync-stdio.ts
+++ packages/@jsii/runtime/lib/sync-stdio.ts
@@ -18,33 +18,41 @@ export class SyncStdio {
   }
 
   public readLine(): string | undefined {
-    if (this.inputQueue.length > 0) {
-      return this.inputQueue.shift();
-    }
-
     const buff = Buffer.alloc(INPUT_BUFFER_SIZE);
-    const read = fs.readSync(STDIN_FD, buff, 0, buff.length, null);
+    while (this.inputQueue.length === 0) {
+      try {
+        const read = fs.readSync(STDIN_FD, buff, 0, buff.length, null);
 
-    if (read === 0) {
-      return undefined;
-    }
+        if (read === 0) {
+          return undefined;
+        }
 
-    const str = buff.slice(0, read).toString();
+        const str = buff.slice(0, read).toString();
 
-    for (const ch of str) {
-      if (ch === '\n') {
-        this.inputQueue.push(this.currentLine);
-        this.currentLine = '';
-      } else {
-        this.currentLine += ch;
+        for (const ch of str) {
+          if (ch === '\n') {
+            this.inputQueue.push(this.currentLine);
+            this.currentLine = '';
+          } else {
+            this.currentLine += ch;
+          }
+        }
+      } catch (e) {
+        // HACK: node may set O_NONBLOCK on it's STDIN depending on what kind of input it is made
+        // of (see https://github.com/nodejs/help/issues/2663). When STDIN has O_NONBLOCK, calls may
+        // result in EAGAIN. In such cases, the call should be retried until it succeeds. This kind
+        // of polling will result in horrible CPU thrashing, but there does not seem to be a way to
+        // force a O_SYNC access to STDIN in a reliable way within node.
+        // In order to make this stop we need to either stop depending on synchronous reads, or to
+        // provision our own communication channel that can reliably be synchronous. This work is
+        // "tracked" at https://github.com/aws/aws-cdk/issues/5187
+        if (e.code !== 'EAGAIN') {
+          throw e;
+        }
       }
     }
 
     const next = this.inputQueue.shift();
-    if (next == null) {
-      return this.readLine();
-    }
-
     return next;
   }
 
